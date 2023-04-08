902a209eb1af616e934fc5ae0df180152050f8e8
Alê <89224363+helloAle@users.noreply.github.com>
2023-04-08 17:58:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const rangeInput = document.getElementById("range-input");
 const output = document.getElementById("output");
+const sortedList = document.getElementById("sorted-list");
 
 rangeInput.addEventListener("input", () => {
   const quantity = rangeInput.value;
   const numbers = Array.from({ length: quantity }, () => Math.floor(Math.random() * 11));
-  output.innerHTML = numbers.map(num => `<span>${num}</span>`).join("");
+  const sortedNumbers = numbers.sort((a, b) => a - b);
+  
+  output.innerHTML = numbers.join(", ");
+  sortedList.innerHTML = sortedNumbers.map(num => `<li>${num}</li>`).join("");
 });
